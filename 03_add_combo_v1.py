8005e0fa973c0c87a19c15aacf6fcbd0437e1185
OnlyNoodles <balakrishn4@middleton.school.nz>
2024-04-12 11:25:06 +1200

    koj

--- 03_add_combo_v1.py
+++
@@ -1,4 +0,0 @@
-"""03_add_combo_v1.
-First version of the 'Add Combo' function.
-"""
-
---
+++ 03_print_menu_v1.py
@@ -0,0 +1,14 @@
+"""03_print_menu_v1.
+First version of the 'Print Menu' function.
+"""
+
+menu = {"Value": {"Beef burger": 5.69, "Fries": 1.00, "Fizzy drink": 1.00},
+        "Cheezy": {"Cheeseburger": 6.69, "Fries": 1.00, "Fizzy drink": 1.00},
+        "Super": {"Cheeseburger": 6.69, "Large fries:": 2.00, "Smoothie": 2.00}}
+
+for combo, item in menu.items():
+    print(f"\nCombo name: {combo}")
+
+    for key in item:
+        print(f"{key}: {item[key]}")
+        
