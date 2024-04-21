b1c2c838594ca73c9733bc59a567e8b0c20c0b0a
OnlyNoodles <balakrishn4@middleton.school.nz>
2024-04-22 01:48:08 +1200

    vcsd

--- 06_add_combo_v1.py
+++ 06_add_combo_v1.py
@@ -1,6 +1,23 @@
-"""06_add_combo_v1."""
+"""06_add_combo_v1.
+Simple code for the adding combo function as this is the first version.
+Modified the menu so that each item is in a category in a combo."""
 
-menu = {"Value": {"Beef burger": 5.69, "Fries": 1.00, "Fizzy drink": 1.00},
-        "Cheezy": {"Cheeseburger": 6.69, "Fries": 1.00, "Fizzy drink": 1.00},
-        "Super": {"Cheeseburger": 6.69, "Large fries:": 2.00, "Smoothie": 2.00}}
+menu = [{"Combo": "Value", "Burger": {"Beef burger": 5.69},
+         "Fries": {"Fries": 1.00}, "Drink": {"Fizzy drink": 1.00}},
+        {"Combo": "Cheezy", "Burger": {"Cheeseburger": 6.69},
+         "Fries": {"Fries": 1.00}, "Drink": {"Fizzy drink": 1.00}},
+        {"Combo": "Super", "Burger": {"Cheeseburger": 6.69},
+         "Fries": {"Large fries": 2.00}, "Drink": {"Smoothie": 2.00}}]
 
+new_combo = {}
+new_combo["Combo"] = input("Enter combo name: ")
+new_combo["Burger"] = {}
+new_combo["Burger"][input("Enter burger: ")] = float(input("Enter burger price: $"))
+new_combo["Fries"] = {}
+new_combo["Fries"][input("Enter size of fries: ")] = float(input("Enter fries price: $"))
+new_combo["Drink"] = {}
+new_combo["Drink"][input("Enter drink: ")] = float(input("Enter drink price: $"))
+
+menu.append(new_combo)
+print("Combo added successfully.")
+print(new_combo)
